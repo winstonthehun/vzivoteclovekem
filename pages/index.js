9e039e7cdc89c77994c41cf9e565f6498eefa851
Winston Sebesta <winston@sebesta.io>
2024-11-12 21:06:03 +0100

    feat: base font

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,21 @@ import Header from '@components/Header'
 import Footer from '@components/Footer'
 import Intro from '@components/intro'
 
+// colors, pink mono
+// ffe5ec
+// ffc2d1
+// ffb3c6
+// ff8fab
+// fb6f92
+
 export default function Home() {
   return (
     <div className="page-container">
       <Head>
         <title>Next.js Starter!</title>
-        {/* <link rel="icon" href="/favicon.ico" /> */}
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Alice&display=swap" rel="stylesheet"/>
       </Head>
 
       <main>
